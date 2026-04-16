1bd4682631027dfd6daf583b94910e08953ced58
UnusualFrog <forwardnoah@gmail.com>
2026-04-16 04:36:25 -0230

    Added shadow to tower

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,10 +3,14 @@ import Image from 'next/image';
 
 export default function Home() {
    return (
-        <div className='flex justify-center align-top'>
-            <div className='bg-red-500 h-auto w-50 ml-10'></div>
-            <div className='bg-blue-500 h-20 w-30 grow ml-10'></div>
-            <div className='bg-blue h-20 w-20'></div>
+    <div className='h-1000'>
+        <div className='flex h-full justify-center align-top'>
+            <div className='bg-pink-t4t w-80 grow ml-20'>
+                <div className='main_tower_in'></div>
+            </div>
+            <div className='bg-pink-t4t w-210 grow ml-40'></div>
+            <div className='bg-blue h-20 w-70 shrink'></div>
         </div>
+    </div>
     );
 }
