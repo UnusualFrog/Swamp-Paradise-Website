42e3987b164c722837403596ea23372d8723fb64
UnusualFrog <forwardnoah@gmail.com>
2026-04-16 20:31:44 -0230

    Created global classes for blogs; reverted use of global classes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,12 +4,19 @@ import Image from 'next/image';
 export default function Home() {
    return (
     <div className='h-1000'>
+        {/* <div className='flex h-full justify-center align-top'> */}
         <div className='flex h-full justify-center align-top'>
             <div className='bg-pink-t4t w-80 grow ml-20'>
-                <div className='main_tower_in'></div>
+                <div className='bg-pink-shadow-t4t h-full w-20 grow'></div>
             </div>
-            <div className='bg-pink-t4t w-210 grow ml-40'></div>
-            <div className='bg-blue h-20 w-70 shrink'></div>
+            <div className='bg-pink-t4t w-200 grow ml-40'>
+                <div className='bg-pink-shadow-t4t w-20 h-full'></div>
+            </div>
+                
+            {/* INVIS */}
+            <div className='bg-blue h-20 w-70 shrink'>
+
+            </div>
         </div>
     </div>
     );
