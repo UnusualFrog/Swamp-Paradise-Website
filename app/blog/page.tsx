3cc5584c2546b81fa09adb5d4d498f39683600a7
UnusualFrog <forwardnoah@gmail.com>
2026-04-16 21:37:42 -0230

    Grid layout applied

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,19 +4,13 @@ import Image from 'next/image';
 export default function Home() {
    return (
     <div className='h-1000'>
-        {/* <div className='flex h-full justify-center align-top'> */}
-        <div className='flex h-full justify-center align-top'>
-            <div className='bg-pink-t4t w-80 grow ml-20'>
-                <div className='bg-pink-shadow-t4t h-full w-20 grow'></div>
-            </div>
-            <div className='bg-pink-t4t w-200 grow ml-40'>
-                <div className='bg-pink-shadow-t4t w-20 h-full'></div>
-            </div>
-                
-            {/* INVIS */}
-            <div className='bg-blue h-20 w-70 shrink'>
+        <div className='h-full grid-container'>
 
-            </div>
+            <div className=' h-full ml-10'></div>
+            <div className='bg-pink-t4t h-full col-start-2 col-end-2 w-100'></div>
+            <div className=' h-full'></div>
+            <div className='bg-pink-t4t h-full col-start-4 col-end-4 w-200 mx-20'></div>
+            <div className='bg-transparent h-full col-start-5 col-end-5 w-20 mr-10'> </div>
         </div>
     </div>
     );
