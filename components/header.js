c88b716230b9ee764f2800197b809d3ac99c8375
UnusualFrog <forwardnoah@gmail.com>
2026-04-16 23:43:38 -0230

    Fixed header image

--- components/header.js
+++ components/header.js
@@ -7,11 +7,12 @@ export default function Header() {
   return (
     <div className='nav'>
             <Image
-                src="/header_lg.bmp"
+                src="/header.bmp"
                 width={0}
                 height={0}
-                alt="Picture of the author"
+                alt="Swamp Paradise"
                 className='header-image'
+                loading='eager'
             ></Image>
             <div className="flex whitespace-nowrap justify-end h-6 mt-6 bg-pink-t4t">
                 <div className='-mt-2'>
