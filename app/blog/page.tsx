32125be021cf365d4f256689e9f7f5d189f3b70f
UnusualFrog <forwardnoah@gmail.com>
2026-04-16 23:36:51 -0230

    Better flexbox towers with shadows

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,13 +4,19 @@ import Image from 'next/image';
 export default function Home() {
    return (
     <div className='h-1000'>
-        <div className='h-full grid-container'>
+        <div className='flex-container'>
 
-            <div className=' h-full ml-10'></div>
+            {/* <div className=' h-full ml-10'></div>
             <div className='bg-pink-t4t h-full col-start-2 col-end-2 w-100'></div>
             <div className=' h-full'></div>
             <div className='bg-pink-t4t h-full col-start-4 col-end-4 w-200 mx-20'></div>
-            <div className='bg-transparent h-full col-start-5 col-end-5 w-20 mr-10'> </div>
+            <div className='bg-transparent h-full col-start-5 col-end-5 w-20 mr-10'> </div> */}
+            <div className='flex-item-1' >
+                <div className='flex-sub-item-1'></div>
+            </div>
+            <div className='flex-item-2'>
+                <div className='flex-sub-item-2'></div>
+            </div>
         </div>
     </div>
     );
